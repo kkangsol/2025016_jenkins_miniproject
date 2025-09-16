18eb1a00713dc147b889dfea103dcbe79fa4c9d5
Kang Hansol <104417575+kkangsol@users.noreply.github.com>
2025-09-16 16:16:30 +0900

    Update Controller.java

--- src/main/java/edu/ce/fisa/controller/Controller.java
+++ src/main/java/edu/ce/fisa/controller/Controller.java
@@ -11,13 +11,13 @@ public class Controller {
 
 	@GetMapping("/get")
 	public String getReqRes() {
-		return "get 방식 요청의 응답 데이터 : 한ㅇ ";
+		return "get 방식 요청의 응답 데이터 : 제현";
 		
 	}
 	
 	//http://localhost:8080
 	@PostMapping("/post")
 	public String getReqRes2() {
-		return "post 방식 요청의 응답 데이터 : 솔한";
+		return "post 방식 요청의 응답 데이터 : 조은";
 	}
 }
